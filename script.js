9675824f1432cac274e6df7ca5adf7808e86d715
marie <ariem.btst@gmail.com>
2025-11-11 02:43:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -62,10 +62,10 @@ function animateText(selector, speed = 200) {
 animateText('header h1', 180);
 
 // ================== APPARITION AU SCROLL ==================
-const faders = document.querySelectorAll('section, header, footer');
+const faders = document.querySelectorAll('#about, #contact');
 
 const appearOptions = {
-  threshold: 0.6,
+  threshold: 0.4,
   rootMargin: "0px 0px -50px 0px"
 };
 
@@ -104,4 +104,3 @@ if (stravaBtn) {
     window.open("https://www.strava.com/athletes/tonprofil", "_blank");
   });
 }
-
